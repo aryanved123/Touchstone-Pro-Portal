22b7f0180bc0636eae94b2802213a6d9b484358f
Aryan Ved <113310577+ak007spongebob@users.noreply.github.com>
2025-04-22 21:39:39 -0400

    added local storage

--- frontend/app.js
+++ frontend/app.js
@@ -2,6 +2,18 @@ angular.module('flagApp', [])
 .controller('MainController', function($scope, $http) {
   $scope.candidate = {};
   $scope.flags = [];
+  // Auto-load form data from localStorage
+  if (localStorage.getItem("candidateData")) {
+    $scope.candidate = JSON.parse(localStorage.getItem("candidateData"));
+  } else {
+    $scope.candidate = {}; // default if nothing saved
+  }
+
+  // Auto-save form to localStorage whenever it changes
+  $scope.$watch('candidate', function(newVal) {
+    localStorage.setItem("candidateData", JSON.stringify(newVal));
+  }, true);
+
 
   $scope.evaluate = function() {
     $http.post('http://localhost:3000/evaluate', $scope.candidate)
